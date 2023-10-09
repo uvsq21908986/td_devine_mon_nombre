dfb0f1af200554658b7dd6e73dec46a95de22956
uvsq21908986 <tom.le-goueff@ens.uvsq.fr>
2023-10-09 11:20:22 +0200

    comparer le nombre saisi avec le nombre généré

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,32 @@
 use std::io;
-use rand::Rng;  // Assurez-vous d'avoir cet import
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Devine mon nombre !");
-    println!("Saisissez votre proposition.");
-
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).unwrap();
-    println!("Vous avez saisi : {}", guess);
-
-    // Cette ligne est maintenant à l'intérieur de la fonction main
     let secret_number = rand::thread_rng().gen_range(1..101);
-    println!("Le nombre secret est : {}", secret_number);
+
+    loop {
+        println!("Saisissez votre proposition.");
+
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).unwrap();
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Veuillez saisir un nombre valide !");
+                continue;
+            }
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Trop petit !"),
+            Ordering::Equal => {
+                println!("Bravo ! Vous avez deviné le nombre.");
+                break;
+            },
+            Ordering::Greater => println!("Trop grand !"),
+        }
+    }
 }
