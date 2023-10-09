93132167e424231ce567c038fd73e494b084c9a0
uvsq21908986 <tom.le-goueff@ens.uvsq.fr>
2023-10-09 22:59:28 +0200

    factorisation du code

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,43 @@
-use std::io;
-use rand::Rng;
 use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    println!("Devine mon nombre !");
-    let secret_number = rand::thread_rng().gen_range(1..101);
+    let secret_number = rand::thread_rng().gen_range(1..101); // Assurez-vous d'avoir importé la crate 'rand'
 
     loop {
-        println!("Saisissez votre proposition.");
+        let input = read_int_from_stdin();
 
-        let mut guess = String::new();
-        io::stdin().read_line(&mut guess).unwrap();
+        if let Some(input) = input { // if_let
+            let comparison = get_ordering(secret_number, input);
+            display_result(comparison);
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Veuillez saisir un nombre valide !");
-                continue;
-            }
-        };
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Trop petit !"),
-            Ordering::Equal => {
-                println!("Bravo ! Vous avez deviné le nombre.");
+            if has_found(comparison) {
                 break;
-            },
-            Ordering::Greater => println!("Trop grand !"),
+            }
+        } else {
+            println!("Saisie incorrecte");
         }
     }
+}
+
+fn read_int_from_stdin() -> Option<u32> {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).ok()?;
+    input.trim().parse().ok()
+}
+
+fn get_ordering(secret_number: u32, input: u32) -> Ordering {
+    secret_number.cmp(&input)
+}
+
+fn display_result(comparison: Ordering) {
+    match comparison {
+        Ordering::Equal => println!("Félicitations! Vous avez trouvé le bon nombre."),
+        Ordering::Less => println!("Trop grand! Essayez un nombre plus petit."),
+        Ordering::Greater => println!("Trop petit! Essayez un nombre plus grand."),
+    }
+}
+
+fn has_found(comparison: Ordering) -> bool {
+    comparison == Ordering::Equal
 }
